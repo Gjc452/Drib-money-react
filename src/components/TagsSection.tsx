ad22722ce75e8537073a0ed1218eeb09f077c18f
Gjc452 <1796444035@qq.com>
2021-02-22 14:03:32 +0800

    完成标签的选择和新增

--- src/components/TagsSection.tsx
+++ src/components/TagsSection.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, {useState} from 'react';
 
 const Wrapper = styled.section`
   background: #FFF;
@@ -15,6 +15,9 @@ const Wrapper = styled.section`
       background: #D9D9D9; border-radius: 18px;
       display: inline-block; padding: 3px 18px;
       font-size: 14px; margin: 8px 8px;
+      &.selected{
+        background: #F60;
+      }
     }
   }
   > button{
@@ -24,15 +27,33 @@ const Wrapper = styled.section`
 `;
 
 const TagsSection: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const onAddTag = () => {
+    const tagName = window.prompt('新标签的名称为');
+    if (tagName !== null) {
+      if (tags.indexOf(tagName) >= 0) {
+        window.alert('标签已存在');
+      } else {
+        setTags([...tags, tagName]);
+      }
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    if (selectedTags.indexOf(tag) >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
   return (
     <Wrapper>
       <ol>
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
+        {tags.map(tag => <li onClick={() => {onToggleTag(tag);}} key={tag}
+                             className={(getClass(tag))}>{tag}</li>)}
       </ol>
-      <button>新增标签</button>
+      <button onClick={onAddTag}>新增标签</button>
     </Wrapper>
   );
 };
