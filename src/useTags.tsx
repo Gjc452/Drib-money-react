de5b671384abe495453678191f96370e8862f13f
Gjc452 <1796444035@qq.com>
2021-02-22 19:50:29 +0800

    tag list 样式完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -2,9 +2,6 @@ import {useState} from 'react';
 
 const useTags = () => {
   const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
-  return {
-    tags,
-    setTags
-  };
+  return {tags, setTags};
 };
 export default useTags;
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,12 +1,53 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import useTags from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+
+const TabList = styled.ol`
+  font-size: 16px;
+  background: white;
+  > li{
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    border-bottom: 1px solid #d5d5d9;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px; border: none;
+  padding: 8px 12px; background: #F60;
+  border-radius: 4px; color: white;
+`;
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Space = styled.div`
+  height: 16px;
+`;
 
 function Tags() {
   const {tags, setTags} = useTags();
   return (
     <Layout>
-      <h2>标签</h2>
+      <TabList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className='oneLine'>{tag}</span>
+            <Icon name='right'/>
+          </li>)}
+      </TabList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新建标签</Button>
+      </Center>
     </Layout>
   );
 }
