549f9fd0e6ad3244d35a8b0290436ef585253ed2
Gjc452 <1796444035@qq.com>
2021-02-22 13:42:13 +0800

    将 Money.tsx 中的四个分区模块化

---
+++ src/components/TagsSection.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const Wrapper = styled.section`
+  background: #FFF;
+  flex-grow: 1;
+  padding: 12px 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  > ol{
+    margin: 0 -12px;
+    >li{
+      background: #D9D9D9; border-radius: 18px;
+      display: inline-block; padding: 3px 18px;
+      font-size: 14px; margin: 8px 8px;
+    }
+  }
+  > button{
+    background: none; border: none; padding: 2px 4px;
+    border-bottom: 1px solid #333; color: #666; margin-top: 8px;
+  }
+`;
+
+const TagsSection: React.FC = () => {
+  return (
+    <Wrapper>
+      <ol>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ol>
+      <button>新增标签</button>
+    </Wrapper>
+  );
+};
+export default TagsSection;
