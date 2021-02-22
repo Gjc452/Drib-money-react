fbeac119a77766611a3fbab5f9b1d26fdd147058
Gjc452 <1796444035@qq.com>
2021-02-22 12:08:38 +0800

    创建 Layout.tsx 组件

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,27 @@ import {
   Link,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Layout from './components/Layout';
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from='/' to='/money'/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from='/' to='/money'/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
@@ -58,15 +41,27 @@ function NoMatch() {
 }
 
 function Money() {
-  return <h2>记账</h2>;
+  return (
+    <Layout>
+      <h2>记账</h2>
+    </Layout>
+  );
 }
 
 function Tags() {
-  return <h2>标签</h2>;
+  return (
+    <Layout>
+      <h2>标签</h2>
+    </Layout>
+  );
 }
 
 function Statistics() {
-  return <h2>统计</h2>;
+  return (
+    <Layout>
+      <h2>统计</h2>
+    </Layout>
+  );
 }
 
 export default App;
